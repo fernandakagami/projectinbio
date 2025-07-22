675d5a888990777100e525a3a792810386e356f8
Fernanda <fernandakagami@gmail.com>
2025-07-22 10:48:42 -0300

    Add button component

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
-    <div className="flex border">Add commentMore actions
-      <div className="w-full flex flex-col gap-2 mt-[35vh] border">
+    <div className="flex border">
+      <div className="w-full flex flex-col gap-2 mt-[35vh]">
         <h1 className="text-5xl font-bold text-white leading-[64px]">Seus projetos e redes sociais em um único link</h1>
         <h2 className="text-xl leading-6">Crie sua própria página de projetos e compartilhe eles com o mundo.
           <br />

c9e20f911abb5a62fe05b7c082a9392be7897022
Fernanda <fernandakagami@gmail.com>
2025-10-21 14:42:08 -0300

    Create profile page

--- app/components/commons/total-visits.tsx
+++ app/components/commons/total-visits.tsx
@@ -1,6 +1,6 @@
 import { TrendingUp } from "lucide-react";
 
-export function TotalVisits() {
+export default function TotalVisits() {
   return (
     <div className="w-min whitespace-nowrap flex items-center gap-5 bg-background-secondary border border-border-primary px-8 py-3 rounded-xl shadow-lg">
       <span className="font-bold text-white">Total de visitas</span>

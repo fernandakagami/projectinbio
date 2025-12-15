7de6959ce227a91b6cca4e81e401963b0c4803da
Fernanda <fernandakagami@gmail.com>
2025-12-15 17:18:29 -0300

    Add modal of new project

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -3,6 +3,10 @@ import TotalVisits from "@/app/components/commons/total-visits";
 import UserCard from "@/app/components/commons/user-card";
 import { Plus } from "lucide-react";
 import Link from "next/link";
+import NewProject from "./new-project";
+import { getProfileData } from "@/app/server/get-profile-data";
+import { notFound } from "next/navigation";
+import { auth } from "@/app/lib/auth";
 
 export default async function ProfilePage({
   params,
@@ -10,6 +14,20 @@ export default async function ProfilePage({
   params: Promise<{ profileId: string }>;
 }) {
   const { profileId } = await params;
+
+  const profileData = await getProfileData(profileId);
+
+  if (!profileData) return notFound();
+
+  // TODO: get projects
+
+  const session = await auth();
+
+  const isOwner = profileData.userId === session?.user?.id;
+
+  // TODO: Adicionar page view
+
+  // TODO: Se o usuario não estiver mais no trial, nao deixar ver o projeto. Redirecionar para upgrade
 
   return (
     <div className="relative h-screen flex p-20 overflow-hidden">
@@ -35,10 +53,8 @@ export default async function ProfilePage({
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
-        <button className="w-[340px] h-[132px] rounded-[20px] bg-background-secondary flex items-center gap-2 justify-center hover:border hover:border-dashed border-border-secondary">
-          <Plus className="size-10 text-accent-green" />
-          <span>Novo projeto</span>
-        </button>
+
+         {isOwner && <NewProject profileId={profileId} />}
       </div>
       <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
         <TotalVisits />
